d652f60af3a84c0ac549f1c53911131821c69490
ebranquinho <branquinhomanu@gmail.com>
2025-09-19 18:47:00 -0300

    add events and delegates

---
+++ AdvancedCSharpProgrammingCourse.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AdvancedCSharpProgrammingCourse.ConsoleApp;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Calculate calculate = Add;
+        var result = calculate(1, 1);
+
+        Console.WriteLine($"The result is: {result}");
+    }
+
+    static double Add(double a, double b) => a + b;
+}
+
+delegate double Calculate(double x, double y);
